7fc2c57a392ab4432881db0b951dea47dcbd71ce
김민준 <79951019+kmj5515@users.noreply.github.com>
2026-04-30 17:58:24 +0900

    Update LastFPS.Build.cs

--- LastFPS/Source/LastFPS/LastFPS.Build.cs
+++ LastFPS/Source/LastFPS/LastFPS.Build.cs
@@ -8,9 +8,12 @@ public class LastFPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"GameplayAbilities", "GameplayTags", "GameplayTasks"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "NetCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
